ce77110812050910549a85897e2afee56b27ebc8
Waldenilson Carneiro <waldenilsonnovaes@gmail.com>
2016-05-06 15:04:32 -0300

    simplificacao das formulas de calculo na def calcular_parcela no modulo calc.py

--- project/calculation/calc.py
+++ project/calculation/calc.py
@@ -7,7 +7,6 @@ def calcular_parcela( parcela ):
 	modulo_fiscal = parcela.pagamento.imovel_titulo.imovel.tamanho_modulo_fiscal
 	valor_imovel = parcela.pagamento.imovel_titulo.valor_imovel
 	data_requerimento = parcela.pagamento.data_requerimento
-	juros = indice_juros(modulo_fiscal=modulo_fiscal, valor_imovel=valor_imovel)
 
 	if data_emissao_titulo > datetime.strptime( '10/02/2009', "%d/%m/%Y").date() and data_emissao_titulo < datetime.strptime( '20/05/2010', "%d/%m/%Y").date():
 		#artigo 12-B
@@ -16,37 +15,19 @@ def calcular_parcela( parcela ):
 		pass
 	elif data_requerimento <= parcela.data_vencimento or (data_requerimento - parcela.data_vencimento).days <= 30:
 		#artigo 8-B alinea a e b
-		vp =  valor_prestacao(prestacao=parcela.valor_principal, data_emissao=data_emissao_titulo, data_requerimento=data_requerimento, juros=juros)
+		#VP = P x ( 1 + N x J/100 )
+		vp =  valor_prestacao(parcela.valor_principal, data_emissao_titulo, data_requerimento, indice_juros(modulo_fiscal, valor_imovel))
 		parcela.valor_juro = vp - float(parcela.valor_principal)
 		parcela.valor_multa = 0.
 		parcela.valor_correcao = 0.
 		parcela.valor_total = vp
 	else:
 		#artigo 8-B alinea c
-		n = prazo_prestacao(data_requerimento=data_requerimento,data_emissao_titulo=data_emissao_titulo)
-		#Na = numero de anos (inteiro) de atraso (desde o vencimento da prestacao)
-		dt = data_requerimento - parcela.data_vencimento
-		qtd_ano = dt.days/360
-		qtd_dias = dt.days - qtd_ano*360
-		na = qtd_ano # 2 anos e 35 dias (data_requerimento - parcela.data_vencimento).days/360
-		#DrA = numero de dias remanescentes (apos se completar a contagem do numero de anos inteiros) ate a data do requerimento mais 30 dias
-		dra = qtd_dias + 30
 		#VPa = P x ( 1 + ( N + Na + DrA/360 ) x J/100 )
-		vpa = valor_prestacao_atraso(prestacao=parcela.valor_principal, n=n, na=na, dra=dra, juros=juros)
-
+		vpa = valor_prestacao_atraso(parcela.valor_principal, data_requerimento, parcela.data_vencimento, data_emissao_titulo, indice_juros(modulo_fiscal, valor_imovel))
 		#artigo 8-C alinea a
-		#CM = porcentagem correspondente a correcao monetaria
-		cm = indice_tr()
-		#Ma = numero de meses (inteiro) de atraso ( decorridos desde o vencimento da prestacao )
-		qtd_mes = dt.days/30
-		qtd_dias = dt.days - qtd_mes*30
-		ma = qtd_mes
-		#DrM = numero de dias remanescentes (apos se completar a contagem de numero de meses inteiros) ate a data do requerimento mais 30 dias
-		drm = qtd_dias + 30 # 5 dias + 30 dias
-		#Jm = taxa de juro mensal de mora
-		jm = 1
 		#VFPa = VPa x ( 1 + CM + ( Ma + DrM/30 ) x Jm/100 )
-		vfpa = valor_final_prestacao_atraso(vpa=vpa, cm=cm, ma=ma, drm=drm, mora=jm)
+		vfpa = valor_final_prestacao_atraso(vpa, data_requerimento, parcela.data_vencimento)
 		parcela.valor_juro = vpa - float(parcela.valor_principal)
 		parcela.valor_multa = jm
 		parcela.valor_correcao = vfpa - vpa
@@ -79,14 +60,33 @@ def valor_prestacao(prestacao, data_emissao, data_requerimento, juros):
 	return float(prestacao) * ( 1 + (float( prazo_prestacao(data_emissao_titulo=data_emissao, data_requerimento=data_requerimento) )/360.)*( juros /100.) )
 
 #VPa
-def valor_prestacao_atraso(prestacao, n, na, dra, juros):
+def valor_prestacao_atraso(prestacao, data_requerimento, data_vencimento, data_emissao_titulo, juros):
 	#artigo 8-B alinea c
+	n = prazo_prestacao(data_requerimento=data_requerimento,data_emissao_titulo=data_emissao_titulo)
+	#Na = numero de anos (inteiro) de atraso (desde o vencimento da prestacao)
+	dt = data_requerimento - parcela.data_vencimento
+	qtd_ano = dt.days/360
+	qtd_dias = dt.days - qtd_ano*360
+	na = qtd_ano # 2 anos e 35 dias (data_requerimento - parcela.data_vencimento).days/360
+	#DrA = numero de dias remanescentes (apos se completar a contagem do numero de anos inteiros) ate a data do requerimento mais 30 dias
+	dra = qtd_dias + 30
 	#VPa = P x ( 1 + ( N + Na + DrA/360 ) x J/100 )
 	return float(prestacao) * ( 1 + ( ( float( n )/360. ) + na + dra/360. ) * ( juros /100.) )
 
 #VFPa
-def valor_final_prestacao_atraso(vpa, cm, ma, drm, mora):
+def valor_final_prestacao_atraso(vpa, data_requerimento, data_vencimento):
 	#artigo 8-C alinea a
+	#CM = porcentagem correspondente a correcao monetaria
+	cm = indice_tr()
+	#Ma = numero de meses (inteiro) de atraso ( decorridos desde o vencimento da prestacao )
+	dt = data_requerimento - data_vencimento
+	qtd_mes = dt.days/30
+	qtd_dias = dt.days - qtd_mes*30
+	ma = qtd_mes
+	#DrM = numero de dias remanescentes (apos se completar a contagem de numero de meses inteiros) ate a data do requerimento mais 30 dias
+	drm = qtd_dias + 30 # 5 dias + 30 dias
+	#Jm = taxa de juro mensal de mora
+	mora = 1
 	#VFPa = VPa x ( 1 + CM + ( Ma + DrM/30 ) x Jm/100 )
 	return vpa * ( 1 + cm + ( ma + float(drm/30.) ) * mora/100.  )
 
